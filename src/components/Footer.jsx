ec796d942b047c5ad505f21abae3d2088d8de2ba
Neeraj-Pant01 <npant923@gmail.com>
2024-04-02 21:42:43 +0530

    added-2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaPhoneAlt } from "react-icons/fa";
 import { IoIosGlasses } from "react-icons/io";
 import { IoLocation } from "react-icons/io5";
+import "./style.css"
 
 
 const Footer = () => {
@@ -12,25 +13,19 @@ const Footer = () => {
             <div>H</div>
             <span>HOTEL<br />MIRANDA</span>
           </div>
-          <p className="section__description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
-            laudantium unde. Doloremque eaque debitis laborum labore voluptates
-            iste molestiae consectetur.
-          </p>
-          <ul className="footer__socials">
-            <li>
-              <a href="#"><i className="ri-youtube-fill"></i></a>
-            </li>
-            <li>
-              <a href="#"><i className="ri-instagram-line"></i></a>
-            </li>
-            <li>
-              <a href="#"><i className="ri-facebook-fill"></i></a>
-            </li>
-            <li>
-              <a href="#"><i className="ri-linkedin-fill"></i></a>
-            </li>
-          </ul>
+
+          <div className="frame">
+          <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13764.072483127351!2d79.30894009904627!3d30.407227752171146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3909d458db25f9b9%3A0x3c8e88f7178344d7!2sGopeshwar%2C%20Uttarakhand%20246401!5e0!3m2!1sen!2sin!4v1707135428328!5m2!1sen!2sin"
+          width="100%"
+          height="400"
+          style={{border: "0"}}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+          </div>
+
         </div>
         <div className="footer__col">
           <h4>Services</h4>
